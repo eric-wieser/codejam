37d4ce1128446fc8113a69b5475581b76d636647
Eric Wieser <wieser.eric@gmail.com>
2015-04-12 01:42:13 +0100

    Rewrite of b

--- 2015-qual/b/main.py
+++ 2015-qual/b/main.py
@@ -3,34 +3,19 @@ import sys
 import math
 from collections import Counter
 
-sys.stdin = open('small.in')
-# sys.stdout = open('small.out', 'w')
+which = 'mysample'
+
+sys.stdin = open('{}.in'.format(which))
+# sys.stdout = open('{}.out'.format(which), 'w')
+
+
 
 def solve(counts):
-	specials = 0
-	times = []
+	return min(
+		sum((c - 1) // stack_size for c in counts) + stack_size
+		for stack_size in range(1, max(counts)+1)
+	)
 
-	counts = Counter(counts)
-
-	print ""
-	while True:
-		first = max(counts)
-		rest = counts - Counter({ first: counts[first] })
-		print "\tCounts:", specials + first, counts
-
-		times.append(specials + first)
-
-		if first <= 3:
-			break
-
-		specials += counts[first]
-		counts = rest + Counter({
-			first - (first // 2): counts[first]
-		}) + Counter({
-			first // 2: counts[first]
-		})
-
-	return min(times)
 
 t = int(raw_input())
 
@@ -39,5 +24,4 @@ for i in range(t):
 	counts = [int(c) for c in raw_input().split()]
 
 	assert len(counts) == d
-
 	print "Case #{}: {}".format(i+1, solve(counts))
