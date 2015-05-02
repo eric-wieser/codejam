0d155781359177ab1112d19548bd924843cdf61a
Eric Wieser <wieser.eric@gmail.com>
2015-05-02 19:04:16 +0100

    Points

--- 2015/1b/b/main.py
+++ 2015/1b/b/main.py
@@ -4,7 +4,7 @@ import math
 from collections import defaultdict
 import numpy as np
 
-which = 'small.0'
+which = 'small.1'
 
 dbg = open('{}.dbg'.format(which), 'w')
 sys.stdin = open('{}.in'.format(which))
@@ -26,6 +26,60 @@ Fails for
 
 """
 
+def tilings(r,c):
+	"""number of gaps - corners, edges, centers, filled"""
+
+	# vals: free, end, corner, edge, center
+
+	if r == 1 and c == 1:
+		yield [1, 0, 0, 0, 0]
+
+	if c == 1:
+		r, c = c, r
+
+	if r == 1:
+		if c % 2 == 0:
+			yield [c//2, 1, c//2 - 1, 0, 0]
+		else:
+			yield [(c+1)//2, 0, (c-1)//2, 0, 0]
+			yield [(c-1)//2, 2, (c-3)//2, 0, 0]
+
+
+	if r % 2 == 0 or c % 2 == 0:
+		vals = [0, 0, 0, 0, 0]
+		vals[2] = 2
+		vals[3] = r - 2 + c - 2
+		if c > 2 and r > 2:
+			vals[4] = (r - 2) * (c - 2) // 2
+
+		vals[0] = r*c - sum(vals[1:])
+
+		yield vals
+	else:
+		vals = [0, 0, 0, 0, 0]
+		# empty corners
+		vals[2]  = 4
+		vals[3] = max(0, r - 3) + max(0, c - 3)
+		if c > 2 and r > 2:
+			vals[4] = ((r - 2) * (c - 2) + 1) // 2
+		vals[0] = r*c - sum(vals[1:])
+
+		yield vals
+
+		vals = [0, 0, 0, 0, 0]
+		# full corners
+		vals[2] = 0
+		vals[3] = r - 1 + c - 1
+		if c > 2 and r > 2:
+			vals[4] = ((r - 2) * (c - 2) - 1) // 2
+		vals[0] = r*c - sum(vals[1:])
+
+		yield vals
+
+
+# print list(tilings(3,5))
+# print list(tilings(1,5))
+
 def solve(r, c, n):
 	r, c = min(r, c), max(r, c)
 	grid = np.ones((r, c), np.int32)
@@ -39,8 +93,9 @@ def solve(r, c, n):
 	print >> dbg, "N:\n", neighbours
 
 	for i in range(0, r*c - n):
+		weights = np.where(grid, neighbours, -1)
 
-		x, y = np.unravel_index(np.where(grid, neighbours, -1).argmax(), grid.shape)
+		x, y = np.unravel_index(weights.argmax(), grid.shape)
 
 		if x+1 < r:  neighbours[x+1, y] -= 1
 		if y+1 < c:  neighbours[x, y+1] -= 1
@@ -57,6 +112,26 @@ def solve(r, c, n):
 
 	return count // 2
 
+def cost(patt, n):
+
+	res = 0
+	for i in range(5):
+		if n <= patt[i]:
+			return res + n*i
+		n -= patt[i]
+		res += patt[i] * i
+
+	raise ValueError
+
+
+
+
+def solve2(r, c, n):
+	patterns = list(tilings(r, c))
+	print >> dbg, patterns
+	return min(cost(p, n) for p in patterns)
+
+
 
 
 t = int(raw_input())
@@ -64,7 +139,7 @@ t = int(raw_input())
 for i in range(t):
 	r, c, n = [int(c) for c in raw_input().split()]
 	print >> dbg, "Case #{}:".format(i+1)
-	res = solve(r, c, n)
+	res = solve2(r, c, n)
 	print "Case #{}: {}".format(i+1, res)
 	print >> dbg, res
 	print >> dbg
--- 2015/1b/c/main.py
+++ 2015/1b/c/main.py
@@ -1,19 +1,47 @@
 from __future__ import division
 import sys
 import math
+from collections import defaultdict, namedtuple
+import numpy as np
+import matplotlib.pyplot as plt
+
+Hiker = namedtuple('Hiker', 'd t')
 
 which = 'sample'
 
+dbg = open('{}.dbg'.format(which), 'w')
 sys.stdin = open('{}.in'.format(which))
-sys.stdout = open('{}.out'.format(which), 'w')
+# sys.stdout = open('{}.out'.format(which), 'w')
 
 
-def solve(counts):
-	pass
+def plot(hikers):
+	t = np.arange(0, max(s for d, s in hikers), min(s for d, s in hikers) / 360)
+	fig, ax = plt.subplots()
+	for h in hikers:
+		ax.plot(t, (h.d + t/h.t * 360) % 360)
+	plt.show()
+
+def solve(hikers):
+	plot(hikers)
+	for at, time in hikers:
+		print at, time
+
 
 
 t = int(raw_input())
 
 for i in range(t):
-	d = int(raw_input())
-	counts = [int(c) for c in raw_input().split()]
+	n = int(raw_input())
+
+	hikers = []
+
+	for _ in range(n):
+		d, h, m = [int(c) for c in raw_input().split()]
+		for duration in range(m, m+h):
+			hikers.append(Hiker(d, duration))
+
+	print >> dbg, "Case #{}:".format(i+1)
+	res = solve(hikers)
+	print "Case #{}: {}".format(i+1, res)
+	print >> dbg, res
+	print >> dbg
